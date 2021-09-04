678d0ceae71fb3860354d9647473c33a06ef123a
Jashawns <shawny583@hotmail.com>
2021-09-04 05:21:21 -0400

    file structure and index adds

---
+++ index.js
@@ -0,0 +1,18 @@
+// Dependencies
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+// Managing Data
+const departments = require('./data/departments');
+const employees = require('./data/employees');
+const roles = require('./data/roles');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'password',
+	database: 'employee_tracker',
+});
+
